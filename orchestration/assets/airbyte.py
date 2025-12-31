210d35f06d2010f4503616d3ca8a6fbd5a8c0c0a
Marlon Moreira <marlon.moreira.runner@gmail.com>
2025-12-31 20:47:01 +1000

    update dagster add dbt assets

--- orchestration/assets/airbyte.py
+++ orchestration/assets/airbyte.py
@@ -1,20 +1,14 @@
 from dagster import EnvVar, AutomationCondition, AssetSpec, AssetKey
 from dagster_airbyte import AirbyteWorkspace, build_airbyte_assets_definitions, DagsterAirbyteTranslator, AirbyteConnectionTableProps
 
-# class CustomDagsterAirbyteTranslator(DagsterAirbyteTranslator):
-#     def get_asset_spec(self, props: AirbyteConnectionTableProps) -> AssetSpec:
-#         default_spec = super().get_asset_spec(props)
-#         return default_spec.replace_attributes(
-#             group_name="airbyte_assets",
-#             # key=AssetKey(["f1", props.table_name]),
-#             automation_condition=AutomationCondition.on_cron(cron_schedule="* * * * *")
-#         )
-
-connections_list = [
-    "bike_store_to_snowflake",
-    "forex_api_to_snowflake",
-    "sentiment_to_snowflake"
-    ]
+class CustomDagsterAirbyteTranslator(DagsterAirbyteTranslator):
+    def get_asset_spec(self, props: AirbyteConnectionTableProps) -> AssetSpec:
+        default_spec = super().get_asset_spec(props)
+        return default_spec.replace_attributes(
+            group_name="airbyte_assets",
+            # key=AssetKey(["f1", props.table_name]),
+            automation_condition=AutomationCondition.on_cron(cron_schedule="* * * * *")
+        )
 
 def get_airbyte_objects():
     # Connect to your OSS Airbyte instance
@@ -27,9 +21,14 @@ def get_airbyte_objects():
     )
 
     # Load all assets from your Airbyte workspace
+    connections_list = [
+    "bike_store_to_snowflake",
+    "forex_api_to_snowflake",
+    "sentiment_to_snowflake"
+    ]
     airbyte_assets = build_airbyte_assets_definitions(
         workspace=airbyte_workspace,
-        # dagster_airbyte_translator=CustomDagsterAirbyteTranslator(),
+        dagster_airbyte_translator=CustomDagsterAirbyteTranslator(),
         connection_selector_fn=lambda connection: connection.name in connections_list
     )
 
