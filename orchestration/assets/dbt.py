d583f8fe12f41d30c0e7d3fbf1f113c0defcac1c
Marlon Moreira <marlon.moreira.runner@gmail.com>
2025-12-31 21:54:14 +1000

    update dagster fic dagster/dagster-dbt versions

--- orchestration/assets/dbt.py
+++ orchestration/assets/dbt.py
@@ -3,27 +3,36 @@ from pathlib import Path
 from dagster_dbt import DbtCliResource, dbt_assets, DagsterDbtTranslator
 from dagster import AssetExecutionContext, AutomationCondition
 
-# configure dbt project resource
-dbt_project_dir = Path(__file__).joinpath("..", "..", "..", "dbt", "warehouse").resolve()
-dbt_warehouse_resource = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
-
-# generate manifest
-if not (dbt_project_dir / "dbt_packages").exists():
-    dbt_warehouse_resource.cli(["deps"]).wait()
-
-dbt_manifest_path = (
-    dbt_warehouse_resource.cli(["parse"])
-    .wait()
-    .target_path
-    .joinpath("manifest.json")
-)
-
-
 class CustomDagsterDbtTranslator(DagsterDbtTranslator):
-    def get_automation_condition(self, dbt_resource_props): 
+    def get_automation_condition(self, dbt_resource_props):
         return AutomationCondition.eager()
 
-# load manifest to produce asset defintion
-@dbt_assets(manifest=dbt_manifest_path, dagster_dbt_translator=CustomDagsterDbtTranslator())
-def dbt_warehouse(context: AssetExecutionContext, dbt_warehouse_resource: DbtCliResource):
-    yield from dbt_warehouse_resource.cli(["build", "--target", "prod"], context=context).stream()
+
+def get_dbt_warehouse_objects():
+    # DBT project directory
+    dbt_project_dir = Path(__file__).joinpath("..", "..", "..", "dbt", "warehouse").resolve()
+
+    # Configure DBT CLI resource
+    dbt_warehouse_resource = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
+
+    # Ensure packages exist
+    if not (dbt_project_dir / "dbt_packages").exists():
+        dbt_warehouse_resource.cli(["deps"]).wait()
+
+    # Generate manifest
+    dbt_manifest_path = (
+        dbt_warehouse_resource.cli(["parse"])
+        .wait()
+        .target_path
+        .joinpath("manifest.json")
+    )
+
+    # Create DBT asset
+    @dbt_assets(
+        manifest=dbt_manifest_path,
+        dagster_dbt_translator=CustomDagsterDbtTranslator()
+    )
+    def dbt_warehouse(context: AssetExecutionContext, dbt_warehouse_resource: DbtCliResource):
+        yield from dbt_warehouse_resource.cli(["build", "--target", "prod"], context=context).stream()
+
+    return dbt_warehouse_resource, dbt_warehouse
--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -1,14 +1,15 @@
 from dagster import Definitions
 from orchestration.assets.airbyte import get_airbyte_objects
-# from orchestration.assets.dbt import dbt_warehouse, dbt_warehouse_resource
+from orchestration.assets.dbt import get_dbt_warehouse_objects
 
 
 airbyte_workspace, airbyte_assets = get_airbyte_objects()
+dbt_warehouse_resource, dbt_warehouse = get_dbt_warehouse_objects()
 
 defs = Definitions(
-    assets=[*airbyte_assets],
+    assets=[*airbyte_assets, dbt_warehouse],
     resources={
         "airbyte": airbyte_workspace,
-        # "dbt_warehouse_resource": dbt_warehouse_resource
+        "dbt_warehouse_resource": dbt_warehouse_resource
         }
 )
